cf81583d70353cf406eae7a0aab3c52e31dc5b05
Kerem Genisel <keremgenisel@gmail.com>
2018-05-28 14:19:17 +0300

    first version

--- SimpleCommerce/Startup.cs
+++ SimpleCommerce/Startup.cs
@@ -60,6 +60,11 @@ namespace SimpleCommerce
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+            name: "areas",
+            template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
