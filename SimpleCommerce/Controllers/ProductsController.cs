35f5e507274de1abed80bf3e3cadc0e8cfbb5429
Kerem Genisel <keremgenisel@gmail.com>
2018-05-31 13:38:16 +0300

    product desc asc

--- SimpleCommerce/Controllers/ProductsController.cs
+++ SimpleCommerce/Controllers/ProductsController.cs
@@ -8,20 +8,42 @@ using System.Threading.Tasks;
 
 namespace SimpleCommerce.Controllers
 {
-    public class ProductsController : ControllerBase { 
-    public ProductsController(ApplicationDbContext context) : base(context)
+    public class ProductsController : ControllerBase
     {
-
-
-    }
-        public IActionResult Index(int CategoryId)
+        public ProductsController(ApplicationDbContext context) : base(context)
         {
-            ViewBag.ProductsCategories = _context.Categories.Include(c =>c.Products).ToList();
+
+
+        }
+        public IActionResult Index(int CategoryId = 0, string order = "date-asc")
+        {
+            ViewBag.ProductsCategories = _context.Categories.Include(c => c.Products).ToList();
             ViewBag.SelectedCategory = _context.Categories.Where(c => c.Id == CategoryId).FirstOrDefault();
             ViewBag.LatestProduct = _context.Products.OrderByDescending(o => o.CreateDate).Take(3).ToList();
-            return View();
+            var products = _context.Products.Where(p => (CategoryId !=0 ? p.CategoryId == CategoryId : true));
+            switch (order)
+            {
+                case "date-asc":
+                    products = products.OrderBy(o => o.CreateDate);
+                    break;
+                case "date-desc":
+                    products = products.OrderByDescending(o => o.CreateDate);
+                    break;
+                case "price-asc":
+                    products = products.OrderBy(o => o.Price);
+                    break;
+                case "price-desc":
+                    products = products.OrderByDescending(o => o.Price);
+                    break;
+
+
+
+
+            }
+
+            return View(products.ToList());
         }
 
     }
-    
+
 }
